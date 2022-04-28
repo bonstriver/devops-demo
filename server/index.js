13979f7d534a96055eb3aa2e57c8522d7bbb9184
bonstriver <brandon.martinez0502@gmail.com>
2022-04-28 11:46:30 -0700

    test revert

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const app = express() CHECK
+const app = express() 
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
